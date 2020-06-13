c8e93acdf87243d9bcc21a7ac7962cdebbb41333
mskfkd <fm6096@yahoo.co.jp>
2020-06-13 11:37:51 +0900

    パスワード間違え時の処理修正&処理終了後再度利用するかの機能追加

--- index.php
+++ index.php
@@ -37,6 +37,18 @@ class ATM {
 	const BALANCE = 1;
 	const DEPOSIT = 2;
 	const WITHDRAWL = 3;
+	const MENU_LIST = array(
+		1 => BALANCE,
+		2 => DEPOSIT,
+		3 => WITHDRAWL
+	);
+
+	const YES = "y";
+	const NO = "n";
+	const RES_MENU = array(
+		"y" => YES,
+		"n" => NO
+	);
 
 	public $getId;
 	public $getPass;
@@ -57,7 +69,7 @@ class ATM {
 		$inputID = rtrim(fgets(STDIN));
 		$inputRes = $this->checkId($inputID);
 		if ($inputRes === false) {
-			return login();
+			return $this->login();
 		}
 
 		//Userクラスのユーザーリストにidがあるかチェック
@@ -71,45 +83,52 @@ class ATM {
 		$user = User::getUserById($inputID);
 		$getId = $user["id"];
 
-        //パスワード取得
-		echo "パスワードを入力してください" . "\n";
-		$inputPass = rtrim(fgets(STDIN));
-		$inputRes = $this->checkPass($inputPass);
+		$this->loginPass();
 
-        //取得したユーザーのパスワードと入力値が一致するかチェック
-            //なければエラー、再帰関数
+//取得したユーザーのパスワードと入力値が一致するかチェック
+        //なければエラー、再帰関数
 		$getPass = $user["password"];
-		var_dump($getPass);
-		if ($getPass !== $inputPass) {
+		if ($getPass !== $this->inputPass) {
 			echo "パスワードが一致しません。" . "\n";
-			return $this->login();
+			return $this->loginPass();
 		}
 
         //問題なければ、プロパティの$userにセット
 		$this->user = $user;
 	}
 
+	public function loginPass() {
+		//パスワード取得
+		echo "パスワードを入力してください" . "\n";
+		$inputPass = $this->inputPass;
+		$this->inputPass = rtrim(fgets(STDIN));
+		$inputRes = $this->checkPass($this->inputPass);
+		if ($this->inputPass === false) {
+			return $this->loginPass();
+		}
+	}
+
 	public function checkId($inputID) {
 		if (!isset($inputID)) {
 			echo "IDを入力してください" . "\n";
-			return $this->login();
+			return false;
 		}
 
 		if (!is_numeric($inputID)) {
 			echo "IDは数字で入力してください" . "\n";
-			return $this->login();
+			return false;
 		}
 	}
 
 	public function checkPass($inputPass) {
 		if (!isset($inputPass)) {
 			echo "パスワードを入力してください" . "\n";
-			return $inputPass;
+			return false;
 		}
 
 		if (!is_numeric($inputPass)) {
 			echo "パスワードは数字で入力してください" . "\n";
-			return $inputPass;
+			return false;
 		}
 	}
 
@@ -129,7 +148,6 @@ class ATM {
 			case self::WITHDRAWL:
 			$this->withdrawl();
 			break;
-
 		}
 	}
 
@@ -142,51 +160,83 @@ class ATM {
 		if($checkres === false) {
 			return $this->selectMenu();
 		}
-		
+
 		return $input;
 	}
 
-	public function check($input) {
-		if (empty($input)) {
+	public function check($input) {//メニュー選択のバリデーション
+		if (!isset($input)) {
 			echo "1~3のいずれかを入力してください" . "\n";
 			return false;
 		}
 
-		if ($input === 0 || $input >= 4) {
+		if (!self::MENU_LIST[$input]) {
+			var_dump(self::MENU_LIST[$input]);
+			var_dump($input);
 			echo "1~3のいずれかで選択し直してください" . "\n";
 			return false;
 		}
-
 		return true;
+
 	}
 
-	public function balance() {
-
+	public function balance() {//残高照会
 		$balance = $this->user["balance"];
 		echo "残高は" . $balance . "円です";
+		$this->replay();
 	}
 
-	public function deposit() {
+	public function deposit() {//入金
 		echo "いくら入金しますか?";
-		$deposit = $this->deposit;
-		$this->deposit = rtrim(fgets(STDIN));
-		echo $this->deposit . "円入金しました" . "\n";
+		$deposit = rtrim(fgets(STDIN));
+		echo $deposit . "円入金しました" . "\n";
 		$balance = $this->user["balance"];
-		$balance = $balance + $this->deposit;
+		$balance = $balance + $deposit;
 		echo "残高は" . $balance . "です。" . "\n";
+		$this->replay();
 	}
 
-	public function withdrawl() {
+	public function withdrawl() {//出金
 		echo "いくら出金しますか?";
-		$withdrawl = $this->withdrawl;
-		$this->withdrawl = rtrim(fgets(STDIN));
-		echo $this->withdrawl . "円引き出しました";
+		$withdrawl = rtrim(fgets(STDIN));
+		echo $withdrawl . "円引き出しました";
 		$balance = $this->user["balance"];
-		$balance = $balance - $this->withdrawl;
+		$balance = $balance - $withdrawl;
 		echo "残高は" . $balance . "です。" . "\n";
+		$this->replay();
 	}
 
-}
+	public function replay() {
+		//機能を引き続き使うか確認
+		echo "他にも手続きされますか? y:はい か n:いいえを入力してください" . "\n";
+		$inputReplay = rtrim(fgets(STDIN));
+
+		//入力内容確認
+		$inputRes = $this->checkReplayRes($inputReplay);
+		if ($inputRes === false) {
+			return $this->replay();
+		}
+
+		if ($inputReplay === self::YES) {
+			return $this->main();
+		}else if($inputReplay === self::NO){
+			echo "ご利用ありがとうございました。" . "\n";
+			return;
+		}
+	}
+
+	public function checkReplayRes($inputReplay) {
+		if (!isset($inputReplay)) {
+			echo "はいであればyを、いいえであればnを入力してください" . "\n";
+			return false;
+		}
+
+		if (!self::RES_MENU[$inputReplay]) {
+			echo "y:はい か n:いいえを入力してください" . "\n";
+			return false;
+		}
+	}
+}//class ATM
 
 $atm = new ATM;
 $atm->main();
